ae8e2741a9e2162bf74407916c3096619dff4d79
Myhjt <970075985@qq.com>
2021-12-28 15:39:38 +0800

    1.新增食物摄入量属性 2.分析页面添加各营养成分含量计算

--- app/src/main/java/com/lnm011223/foods_secret/logic/model/FoodResponse.kt
+++ app/src/main/java/com/lnm011223/foods_secret/logic/model/FoodResponse.kt
@@ -26,15 +26,17 @@ data class FoodResponse(val code:String, @SerializedName("newslist")val FoodInfo
  * @param carbohydrate 碳水化合物
  * @param fat 脂肪
  * @param diaryFiber 膳食纤维
+ * @param intakeAmount 摄入量（克），默认100
  */
 @Parcelize
 data class Food(val name:String="",
                 var isSelected:Boolean=true,
-                @SerializedName("rl")val power:Float=0.0F,
+                val intakeAmount:Int = 100,
+                @SerializedName("rl") var power:Float=0.0F,
                 @SerializedName("dbz")val protein:Float=0.0F,
-                @SerializedName("zf")val fat:Float=0.0F,
-                @SerializedName("shhf")val carbohydrate:Float=0.0F,
-                @SerializedName("ssxw")val diaryFiber:Float=0.0F):Parcelable {
+                @SerializedName("zf") var fat:Float=0.0F,
+                @SerializedName("shhf") var carbohydrate:Float=0.0F,
+                @SerializedName("ssxw") var diaryFiber:Float=0.0F):Parcelable {
     override fun equals(other: Any?): Boolean {
         if (other != null) {
             return if (other::class.java == Food::class.java) {
--- app/src/main/java/com/lnm011223/foods_secret/ui/analyse/AnalyseFragment.kt
+++ app/src/main/java/com/lnm011223/foods_secret/ui/analyse/AnalyseFragment.kt
@@ -7,16 +7,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.lnm011223.foods_secret.R
 import com.lnm011223.foods_secret.databinding.AnalyseFragmentBinding
 import com.lnm011223.foods_secret.logic.model.Food
 import kotlinx.android.synthetic.main.search_fragment.*
 
+@Suppress("UNCHECKED_CAST")
 class AnalyseFragment : Fragment() {
 
     private lateinit var analyseViewModel: AnalyseViewModel
     private var _binding: AnalyseFragmentBinding? = null
-
+    private lateinit var totalFood:Food
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -44,9 +46,21 @@ class AnalyseFragment : Fragment() {
 
         // 获取选择的搭配
         val foodList:ArrayList<Food> = arguments?.getSerializable("foodList") as ArrayList<Food>
+        totalFood = Food()
+
+        // 计算各营养成分的含量
         for (food in foodList) {
-            Log.i("Test", food.toString());
+            val intake = food.intakeAmount / 100.0F
+            totalFood.carbohydrate  += intake * food.carbohydrate
+            totalFood.diaryFiber += intake * food.diaryFiber
+            totalFood.fat += intake * food.fat
+            totalFood.power += intake * food.power
         }
+        Log.i("TEST", "onActivityCreated: 总的摄入量$totalFood");
     }
 
+    // 分析
+    private fun analyze() {
+
+    }
 }
