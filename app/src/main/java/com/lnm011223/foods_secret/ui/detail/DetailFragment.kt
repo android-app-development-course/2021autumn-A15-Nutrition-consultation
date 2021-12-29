5e61bf095d29f21af1ba57f661175cf59eb490e5
lnm011223 <2969827073@qq.com>
2021-12-29 14:43:46 +0800

    完善分类详情

--- app/src/main/java/com/lnm011223/foods_secret/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/lnm011223/foods_secret/ui/detail/DetailFragment.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,15 +17,19 @@ import com.lnm011223.foods_secret.logic.model.Food
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.view.*
 import kotlinx.android.synthetic.main.detail_fragment.*
+import androidx.recyclerview.widget.RecyclerView
+
+
+
 
 /**
  * 某个分类下的食物列表页
  */
 class DetailFragment : Fragment() {
-
+    var foodCategoryName = ""
     private lateinit var detailViewModel: DetailViewModel
     private var _binding: DetailFragmentBinding? = null
-
+    var page_num = 2
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -58,10 +63,10 @@ class DetailFragment : Fragment() {
         val adapter = FoodAdapter(foodList)
         binding.recyclerView.adapter = adapter
 
-        val foodCategoryName = view_foodCategoryName.text.toString()
+        foodCategoryName = view_foodCategoryName.text.toString()
         if (foodCategoryName.isNotEmpty()) {
             // 发起请求获取食物列表
-            detailViewModel.getFoodList(foodCategoryName, 2)
+            detailViewModel.getFoodList(foodCategoryName, 1)
         }
         // 查看请求响应
         detailViewModel.foodListLiveData.observe(viewLifecycleOwner, {
@@ -80,7 +85,140 @@ class DetailFragment : Fragment() {
         }
 
 
+
+        binding.recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                //Log.i("page_test", "--------------------------------------")
+                if (binding.recyclerView.canScrollVertically(1)) {
+                    //Log.i(TAG, "direction 1: true")
+                } else {
+                    Log.i("page_test", "direction 1: false") //滑动到底部
+                    page_num++
+                    Log.i("page_test","page = $page_num")
+                    detailViewModel.getFoodList(foodCategoryName, page_num)
+                    jumptofirst()
+                }
+                if (binding.recyclerView.canScrollVertically(-1)) {
+                    //Log.i(TAG, "direction -1: true")
+                } else {
+                    Log.i("page_test", "direction -1: false") //滑动到顶部
+                    page_num--
+                    Log.i("page_test","page = $page_num")
+                    detailViewModel.getFoodList(foodCategoryName, page_num)
+                    jumptofirst()
+                }
+            }
+        })
+
     }
 
+    private fun jumptofirst(){
+        when(foodCategoryName) {
+            //max=5
+            "谷类" ->{
+                Log.d("page_test",foodCategoryName)
+                if (page_num>5) {
+                    page_num = 1
+                    detailViewModel.getFoodList(foodCategoryName, page_num)
+                    Log.d("page_test", "$foodCategoryName jump succeed")
+                }
+            }
+            //max=4
+            "干豆类" ->{
+                Log.d("page_test",foodCategoryName)
+                if (page_num>4) {
+                    page_num = 1
+                    detailViewModel.getFoodList(foodCategoryName, page_num)
+                    Log.d("page_test", "$foodCategoryName jump succeed")
+                }
+            }
+            //max==12
+            "蔬菜类"->{
+                Log.d("page_test",foodCategoryName)
+                if (page_num>12) {
+                    page_num = 1
+                    detailViewModel.getFoodList(foodCategoryName, page_num)
+                    Log.d("page_test", "$foodCategoryName jump succeed")
+                }
+            }
+            //max=9
+            "水果类"->{
+                Log.d("page_test",foodCategoryName)
+                if (page_num>9) {
+                    page_num = 1
+                    detailViewModel.getFoodList(foodCategoryName, page_num)
+                    Log.d("page_test", "$foodCategoryName jump succeed")
+                }
+            }
+            //max=7
+            "畜肉类"->{
+                Log.d("page_test",foodCategoryName)
+                if (page_num>7) {
+                    page_num = 1
+                    detailViewModel.getFoodList(foodCategoryName, page_num)
+                    Log.d("page_test", "$foodCategoryName jump succeed")
+                }
+            }
+            //max=3
+            "禽肉类"->{
+                Log.d("page_test",foodCategoryName)
+                if (page_num>3) {
+                    page_num = 1
+                    detailViewModel.getFoodList(foodCategoryName, page_num)
+                    Log.d("page_test", "$foodCategoryName jump succeed")
+                }
+            }
+            //max=2
+            "乳类"->{
+                Log.d("page_test",foodCategoryName)
+                if (page_num>2) {
+                    page_num = 1
+                    detailViewModel.getFoodList(foodCategoryName, page_num)
+                    Log.d("page_test", "$foodCategoryName jump succeed")
+                }
+            }
+            //max=2
+            "蛋类"->{
+                Log.d("page_test",foodCategoryName)
+                if (page_num>2) {
+                    page_num = 1
+                    detailViewModel.getFoodList(foodCategoryName, page_num)
+                    Log.d("page_test", "$foodCategoryName jump succeed")
+                }
+            }
+            //max=8
+            "鱼虾蟹贝"->{
+                Log.d("page_test",foodCategoryName)
+                if (page_num>8) {
+                    page_num = 1
+                    detailViewModel.getFoodList(foodCategoryName, page_num)
+                    Log.d("page_test", "$foodCategoryName jump succeed")
+                }
+            }
+            //max=2
+            "油脂类"->{
+                Log.d("page_test",foodCategoryName)
+                if (page_num>2) {
+                    page_num = 1
+                    detailViewModel.getFoodList(foodCategoryName, page_num)
+                    Log.d("page_test", "$foodCategoryName jump succeed")
+                }
+            }
+            //max=1
+            "薯类淀粉"->{
 
+            }
+            //max=3
+            "坚果种子"->{
+                Log.d("page_test",foodCategoryName)
+                if (page_num>3) {
+                    page_num = 1
+                    detailViewModel.getFoodList(foodCategoryName, page_num)
+                    Log.d("page_test", "$foodCategoryName jump succeed")
+                }
+            }
+        }
+    }
 }
